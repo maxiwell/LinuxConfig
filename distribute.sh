7478bc3ab3c3472d766d5eb8156de200215057a5
Maxiwell <maxiwell@notelab>
2012-12-07 16:09:51 -0200

    distribute modify

---
+++ distribute.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+allfil=`find ./home/*/ -type f`
+alllnk=`find ./home/*/ -type l`
+
+for k in $allfil; do
+	dir=`echo $k | sed -e "s/\/[^\/]*$/\//" | sed -e "s/^\.//"`
+	fil=`echo $k | sed -e "s/.*\///"`
+	if [ $fil == ".gitconfig" ]; then
+		echo "skipping .gitconfig"
+		continue
+	fi
+	if [ "$1" != "-p" ]; then
+		cp ./$dir$fil $dir$fil
+	fi
+	if [ "$1" == "-v" ]; then
+		echo "cp ./$dir$fil $dir$fil"
+	fi
+done
+
+for k in $alllnk; do
+	pat=`echo $k | sed -e "s/\/[^\/]*$/\//" | sed -e "s/^\.//"`
+	lnk=`echo $k | sed -e "s/.*\///"`
+	if [ "$1" != "-p" ]; then
+		cp -d ./$pat$lnk $pat$lnk
+	fi
+	if [ "$1" == "-v" ]; then
+		echo "cp -d ./$pat$lnk $pat$lnk"
+	fi
+done
+
+exit 0
