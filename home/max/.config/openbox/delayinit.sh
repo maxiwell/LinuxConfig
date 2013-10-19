cf25cceafd8a716865fed0faffb288b910b0796c
maxiwell <maxiwell@gmail.com>
2013-10-19 17:49:40 -0300

    New themes of tint2, with lauchers.

--- home/max/.config/openbox/delayinit.sh
+++ home/max/.config/openbox/delayinit.sh
@@ -2,11 +2,12 @@
 
 
 #(sleep 10 && feh --bg-tile Pictures/Emily_Blunt.jpg) &
-sleep 2
+sleep 1
 
 #nm-applet & 
 #/home/max/.dzenconky.sh &
 #conky -c /home/max/.conky/meu2.conkyrc &
+hdmiclone
 tint2 &
-#gnome-volume-control-applet &
 gnome-sound-applet &
+
