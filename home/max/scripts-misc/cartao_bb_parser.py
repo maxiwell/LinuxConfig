0afc9b0aa19802d59488b7703bbd7aa866b406dc
maxiwell <maxiwell@gmail.com>
2013-11-01 14:33:27 -0200

    Python BB code improved

--- home/max/scripts-misc/cartao_bb_parser.py
+++ home/max/scripts-misc/cartao_bb_parser.py
@@ -37,22 +37,81 @@ places_dic = {
 'PIER 4' : 'Comida',
 'GOOGLE' : 'Tecnologia',
 'RECARGA TIM' : 'Celular:TIM',
-'HENRIQUE MAT' : 'Tecnologias'
-
+'HENRIQUE MAT' : 'Tecnologias',
+'AJUSTE' : 'Banco',
+'REDE CAMPEAO' : 'Carro:Combustivel',
+'CARNES AMERICA' : 'Mercado'
 }
 
 
 def get_cotacao(i):
-        lsplit = i.next()
-        while (lsplit[0].find("convertido") == -1):
-            lsplit = i.next().split("\t")
-        
-        # escape the dotted line
-        i.next().split()
-        
-        lsplit = i.next().split()
-        cotacao_dolar = lsplit[-3]
-        return cotacao_dolar.replace(",", ".")
+    lsplit = i.next()
+    while (lsplit[0].find("convertido") == -1):
+        lsplit = i.next().split("\t")
+    
+    # escape the dotted line
+    i.next().split()
+    
+    lsplit = i.next().split()
+    cotacao_dolar = lsplit[-3]
+    return cotacao_dolar.replace(",", ".")
+
+
+# Tipo: pode ser Compras ou Creditos
+def dump(i, tipo):
+    while True:
+       try:
+           lsplit = i.next().split()
+
+           if (not lsplit):
+               return
+
+           if (lsplit[0].find("/") == -1):
+               continue
+                   
+           data = lsplit[0].replace("/","-")
+           # set list in string, with whitespace between each element
+           descricao = ' '.join(lsplit[1:-4])
+
+           pais = lsplit[-3]
+           if (pais != "BR"):
+               if (lsplit[-1] == "0,00"):
+                   valor = lsplit[-2]
+                   cotacao_dolar_temp = 1;  # Valor ja convertido pela administradora
+               else:
+                   valor = lsplit[-1]
+                   cotacao_dolar_temp = cotacao_dolar
+
+               valor = float(format(float(valor.replace(",",".")), '.2f'))*-1
+   
+               if (tipo == "Compras"):
+                  valor = float(format(((valor * IOF)+valor), '.2f'))
+
+               valor = str(valor * cotacao_dolar_temp).replace(".", ",")
+
+               # Truncando o valor duas casas apos o ponto. Arredontamento nao pode!
+               dot_index = valor.find(",")
+               valor = valor[0:dot_index+3]
+
+               if (tipo == "Compras"):
+                   cidade = "Cotacao U$: "+str(cotacao_dolar)
+           else:
+               cidade = lsplit[-4]
+               valor = str(float(lsplit[-2].replace(",","."))*-1).replace(".",",")
+
+           line = data+";1;;;"+descricao+", "+cidade+";"+valor+";"
+         
+           # search the place name in the key 
+           for key in places_dic:
+               if re.match(".*"+key, descricao):
+                   line = line+str(places_dic.get(key))
+                   break
+
+           result.write(line+";\n")
+       except StopIteration:
+           break
+
+          
 
 #----------------
 # Main
@@ -63,76 +122,35 @@ if len(sys.argv) <= 1 :
 else:
     print 'Carregando: ', sys.argv[1]
 
-result = open(sys.argv[1]+".csv", "w")
+result = open(sys.argv[1]+".csv", "r+w")
 
 cartao = open(sys.argv[1], 'r')
 readingFile = cartao.read()
 readingFile = readingFile.split('\n')
 
-i = iter(readingFile)
-lsplit = i.next()
-while (lsplit[0].find("Compras a vista") == -1):
-    lsplit = i.next().split("\t")
-
-
 i_temp = iter(readingFile)
 cotacao_dolar = float(format(float(get_cotacao(i_temp)),'.4f'))
 IOF = 0.0638
 
-while True:
-    try:
-        lsplit = i.next().split()
+i = iter(readingFile)
+lsplit = i.next()
+while ((lsplit[0].find("Compras a vista") == -1) and (lsplit[0].find("Creditos diversos") == -1)):
+    lsplit = i.next().split("\t")
 
-        if (not lsplit):
-            break
-
-        if (lsplit[0] == "**"):
-            continue
-        
-       
-        data = lsplit[0].replace("/","-")
-        # set list in string, with whitespace between each element
-        descricao = ' '.join(lsplit[1:-4])
-
-        pais = lsplit[-3]
-        if (pais != "BR"):
-            if (lsplit[-1] == "0,00"):
-                valor = lsplit[-2]
-                cotacao_dolar_temp = 1;  # Valor ja convertido pela administradora
-            else:
-                valor = lsplit[-1]
-                cotacao_dolar_temp = cotacao_dolar
-
-            valor = float(format(float(valor.replace(",",".")), '.2f'))
-            valor = float(format(((valor * IOF)+valor), '.2f'))
-            valor = str(valor * cotacao_dolar_temp).replace(".", ",")
-
-            # Truncando o valor duas casas apos o ponto. Arredontamento nao pode!
-            dot_index = valor.find(",")
-            valor = valor[0:dot_index+3]
-            cidade = "Cotacao U$: "+str(cotacao_dolar)
-        else:
-            cidade = lsplit[-4]
-            valor = lsplit[-2] 
-
-        line = data+";1;;;"+descricao+", "+cidade+";-"+valor+";"
-      
-        # search the place name in the key 
-        for key in places_dic:
-            if re.match(".*"+key, descricao):
-                line = line+str(places_dic.get(key))
-                break
-
-        result.write(line+";\n")
- #       print line
-    except StopIteration:
-        break
-
-
-
+if (lsplit[0].find("Creditos diversos")):
+    dump(i, "Creditos");
+dump(i, "Compras");
 
 cartao.close()
+
+# Checando o Valor total
+somatorio = 0.0;
+for line in result:
+     somatorio += float(line.split(";")[5].replace(",","."))
+print "Total da Fatura: R$ " + str(somatorio)
+
 result.close()
+
 
 
 
