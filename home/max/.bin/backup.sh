35ca00330a286a26e3d662dcf0be50d3818f982c
maxiwell <maxiwell@gmail.com>
2013-11-28 04:25:44 -0200

    Backup script with -exclude option

--- home/max/.bin/backup.sh
+++ home/max/.bin/backup.sh
@@ -17,8 +17,29 @@ do
     [[ $line == \[* ]] && SERVER=`echo $line | cut -d "[" -f2 | cut -d "]" -f1` && continue
 
     [[ $SERVER == "" ]] && echo "[Server] not found in ~/.backup to $line" && continue
-    if [ -d "$line" ]; then
-        rsync -CRravzp -e ssh $line $SERVER
+    if [ `echo $line | wc -w` -gt 1 ]; then
+       P1=`echo $line | cut -d\  -f1`
+       ARG=`echo $line | cut -d\  -f2`
+       EXCLUDE_LIST=`echo $line | cut -d\  -f3-`
+       if [ $ARG != "-exclude" ]; then 
+            echo "Argument $ARG dont implemented in $line"
+            continue
+       fi
+       rm -f excluded.txt  # just in case
+       for x in $EXCLUDE_LIST
+       do
+           echo $x >> excluded.txt
+       done
+       if [ -e "$P1" ]; then
+           echo -e "\n[RSYNC] $P1 -> $SERVER [EXCLUDE] $EXCLUDE_LIST"
+           rsync -CRravzp --delete --exclude-from excluded.txt -e ssh $P1 $SERVER
+       else
+           echo -e "ERROR: The path '$line' don't exists\n"
+       fi
+       rm -f excluded.txt
+    elif [ -e "$line" ]; then
+        echo -e "\n[RSYNC] $line -> $SERVER"
+        rsync -CRravzp --delete -e ssh $line $SERVER
     else
         echo -e "ERROR: The path '$line' don't exists\n"
     fi
