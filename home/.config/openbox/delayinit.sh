6487f7af3d5d1917350628b8f924d86b212fa42a
maxiwell <maxiwell@gmail.com>
2014-06-17 14:49:50 -0300

    update

--- home/.config/openbox/delayinit.sh
+++ home/.config/openbox/delayinit.sh
@@ -4,20 +4,12 @@
 hdmi clone &
 tint2 &
 gnome-sound-applet &
+volumeicon &
 
 #feh --bg-tile Pictures/Emily_Blunt.jpg &
 
-sleep 2
+sleep 1
 conky  -c $HOME/.conky/conkyrc_seamod &
 
 killall ssh-agent
-
-#conky  -c $HOME/.conky/conkyrc_conn &
-
-# I mounted with etc/fstab because samba doesn't works with udisks 
-#if [[ `uname -n` == "max-p150hm" ]]; then
-#    udisks --mount /dev/sdc1 --mount-options umask=0007
-#    udisks --mount /dev/sdd1 --mount-options umask=0007
-#    udisks --mount /dev/sde1 --mount-options umask=0007
-#fi
 
