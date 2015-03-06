8bb6777f27f83f0fabf912c4c66a69256b404e27
maxiwell <maxiwell@gmail.com>
2015-03-06 01:23:01 -0300

    cartao_ofx.sh: added dolar price in the memo entry

--- home/scripts-misc/cartao_ofx.sh
+++ home/scripts-misc/cartao_ofx.sh
@@ -4,19 +4,28 @@ FILE=$1
 
 
 echo "Type the conversion BRL->USD: "
-read CONVERTION
+read CONVERSION
 
-cp $FILE ${FILE}_bkp
-grep -n -B 1 USD $FILE | while read -r GREPLINE; do
+cp $FILE output_${FILE}
+FILE="output_${FILE}"
+grep -n -B 1 -A 2 USD ${FILE} | while read -r GREPLINE; do
 
     # Get the GREP output that contains the Currency Value
     if [[ $GREPLINE == *"TRNAMT"* ]]; then
        LINENUMBER=`echo $GREPLINE | cut -d- -f 1`
        VALUESTR=`echo $GREPLINE | cut -d\> -f 2 | cut -d\< -f 1`
 
-       VALUECONV=`echo $VALUESTR*$CONVERTION | bc`
+       VALUECONV=`echo $VALUESTR*$CONVERSION | bc`
 
        sed -i "${LINENUMBER}s/${VALUESTR}/${VALUECONV}/" $FILE 
+    fi
+    # Get the GREP output that contains the MEMO information and 
+    # annotates conversion
+    if [[ $GREPLINE == *"MEMO"* ]]; then
+        LINENUMBER=`echo $GREPLINE | cut -d- -f 1`
+
+        sed -i "${LINENUMBER}s@<MEMO>@<MEMO>$VALUESTR*$CONVERSION | @" $FILE
+
     fi
 done
 
