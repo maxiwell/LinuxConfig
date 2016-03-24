c5569d70609e91dbbb2c5de203e2c8bdebc1a693
maxiwell <maxiwell@gmail.com>
2016-03-24 00:56:16 -0300

    [pa-vol.sh] Refactoring code

--- home/.local/bin/pa-vol.sh
+++ home/.local/bin/pa-vol.sh
@@ -17,28 +17,22 @@ VOL_NOW=`pacmd dump | grep -P "^set-sink-volume $SINK_NAME\s+" | perl -p -i -e '
 case "$1" in
 	plus)
 	VOL_NEW=$((VOL_NOW + VOL_STEP))
-	if [ $VOL_NEW -gt $((0x10000)) ]
-	then
-		VOL_NEW=$((0x10000))
-	fi
+    [[ $VOL_NEW -gt $((0x10000)) ]] && VOL_NEW=$((0x10000))
 	pactl set-sink-volume $SINK_NAME `printf "0x%X" $VOL_NEW`
-
 	;;
-	minus)
+	
+    minus)
 	VOL_NEW=$((VOL_NOW - VOL_STEP))
-	if [ $(($VOL_NEW)) -lt $((0x00000)) ]
-	then
-		VOL_NEW=$((0x00000))
-	fi
+	[[ $(($VOL_NEW)) -lt $((0x00000)) ]] && VOL_NEW=$((0x00000))
 	pactl set-sink-volume $SINK_NAME `printf "0x%X" $VOL_NEW`
 	;;
-	mute)
-
+	
+    mute)
 	MUTE_STATE=`pacmd dump | grep -P "^set-sink-mute $SINK_NAME\s+" | perl -p -i -e 's/.+\s(yes|no)$/$1/'`
-	if [ $MUTE_STATE = no ]
+	if [ $MUTE_STATE = no ];
 	then
 	        pactl set-sink-mute $SINK_NAME 1
-	elif [ $MUTE_STATE = yes ]
+	elif [ $MUTE_STATE = yes ];
 	then
 		pactl set-sink-mute $SINK_NAME 0
 	fi
