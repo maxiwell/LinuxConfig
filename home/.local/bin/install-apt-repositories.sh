499d336dcd0a5346a9f5f3a3067372404611d279
maxiwell <maxiwell@gmail.com>
2016-04-29 00:35:43 -0300

    Updating apt, tmux and bookmarks

--- home/.local/bin/install-apt-repositories.sh
+++ home/.local/bin/install-apt-repositories.sh
@@ -21,4 +21,7 @@ echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) co
 wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add - 
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
 
+# Dropbox
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+echo "deb http://linux.dropbox.com/ubuntu/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/dropbox.list
 
