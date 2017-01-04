f4c244d6586338ae22abfa570c297e566b7ffa98
Maxiwell Garcia <maxiwell@gmail.com>
2017-01-04 16:16:23 -0200

    [ansible] Resolve local user in 'sudo' mode using --extra-vars

--- home/.config/ansible/ansible.sh
+++ home/.config/ansible/ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ansible-playbook -i "localhost," -c local playbook.yml $1 
+sudo ansible-playbook -i "localhost," -c local  playbook.yml --extra-vars "local_user=$USER local_home=$HOME" $1
