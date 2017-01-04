d554a5137182d92fca87982c649c6edf80c06bd1
Maxiwell Garcia <maxiwell@gmail.com>
2017-01-04 18:18:11 -0200

    [ansible] Check if ansible is installed

--- home/.config/ansible/ansible.sh
+++ home/.config/ansible/ansible.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+command -v ansible &>/dev/null || { echo -e "ERROR: Install 'ansible' package and try again: 
+                                            \n\tsudo apt-get install ansible\n"; exit 1; }
+
+
 sudo ansible-playbook -i "localhost," -c local  playbook.yml --extra-vars "local_user=$USER local_home=$HOME" $1
