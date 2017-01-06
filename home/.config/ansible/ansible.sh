9bc677f7b074ed3ec0f7719f3aba58c41837ede0
Maxiwell Garcia <maxiwell@gmail.com>
2017-01-06 13:38:45 -0200

    [ansible] Add a note in script

--- home/.config/ansible/ansible.sh
+++ home/.config/ansible/ansible.sh
@@ -3,5 +3,5 @@
 command -v ansible &>/dev/null || { echo -e "ERROR: Install 'ansible' package and try again: 
                                             \n\tsudo apt-get install ansible\n"; exit 1; }
 
-
+# FIXME: I use 'sudo' because 'pip' module fails  with 'become' privilege escalation
 sudo ansible-playbook -i "localhost," -c local  playbook.yml --extra-vars "local_user=$USER local_home=$HOME" $1
