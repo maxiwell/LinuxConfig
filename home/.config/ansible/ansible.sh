7e93b2e43365f02a374521d853d79c1587e78de1
Maxiwell Garcia <maxiwell@gmail.com>
2017-01-28 16:52:24 -0200

    [ansible] Find all yml files in the dir

--- home/.config/ansible/ansible.sh
+++ home/.config/ansible/ansible.sh
@@ -3,5 +3,9 @@
 command -v ansible &>/dev/null || { echo -e "ERROR: Install 'ansible' package and try again: 
                                             \n\tsudo apt-get install ansible\n"; exit 1; }
 
-# FIXME: I use 'sudo' because 'pip' module fails  with 'become' privilege escalation
-sudo ansible-playbook -i "localhost," -c local  playbook.yml --extra-vars "local_user=$USER local_home=$HOME" $1
+
+for i in *.yml; do
+    # FIXME: I use 'sudo' because 'pip' module fails  with 'become' privilege escalation
+    sudo ansible-playbook -i "localhost," -c local  $i --extra-vars "local_user=$USER local_home=$HOME" $1
+done
+
