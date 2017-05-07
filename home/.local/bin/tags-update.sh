4752f8a3a3c8eba210bc4564fb6971a6428b97df
Maxiwell Garcia <maxiwell@gmail.com>
2017-05-06 22:46:06 -0300

    [tags-update.sh] Add .c support

--- home/.local/bin/tags-update.sh
+++ home/.local/bin/tags-update.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Only cpp/cc is important here. The YouCompleteMe will take care of the rest =)
-find `pwd` -iname *.cpp -o -iname *.cc | grep -v build\/ > tags.files
-ctags --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -L tags.files -o ctags
+# Only c/cpp/cc is important here. The YouCompleteMe will take care of the rest =)
+if [[ "$1" == "c" ]]; then
+    find `pwd` -iname '*.c' | grep -v build\/ > tags.files
+    CTAGS_EXTRA_ARGS=
+else
+    find `pwd` -iname *.cpp -o -iname *.cc | grep -v build\/ > tags.files
+    CTAGS_EXTRA_ARGS="--language-force=C++"
+fi
+
+ctags --c++-kinds=+p --fields=+iaS --extra=+q $CTAGS_EXTRA_ARGS -L tags.files -o ctags
 cscope -b -q -i tags.files
-rm tags.files
 
