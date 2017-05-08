bf940b861b94024bb9618d69c4a7d1ff66713fbc
Maxiwell Garcia <maxiwell@gmail.com>
2017-05-07 23:35:10 -0300

    [bin] Update the book_dropbox.sh script

--- home/.local/bin/books_dropbox.sh
+++ home/.local/bin/books_dropbox.sh
@@ -5,24 +5,16 @@ pdfs=/home/max/Dropbox/pdfs/
 
 allbooks=/files/backup/powernote/media/files/livros/
 
-cd $ebooks 
-for i in *; 
-    do 
-        if test $( find $allbooks -iname "$i" | wc -l) -eq 0 ; then 
-            echo $i; 
-        fi; 
-    done
-cd - &> /dev/null
-
-cd $pdfs
-for i in *; 
-    do 
-        if test $( find $allbooks -iname "$i" | wc -l) -eq 0 ; then 
-            echo $i; 
-        fi ; 
-    done
-cd - &> /dev/null
-
-
+for dir in $ebooks $pdfs;
+do
+    cd $dir
+    for i in *; 
+        do 
+            if test $( find $allbooks -iname "$i" | wc -l) -eq 0 ; then 
+                echo $i; 
+            fi; 
+        done
+    cd - &> /dev/null
+done
 
 
