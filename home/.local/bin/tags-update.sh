7dc3cb2c8a1a2d0ef61f9b8cfb0ec52b1c31e57a
Maxiwell Garcia <magarcia@padtec.com.br>
2017-05-09 14:48:44 -0300

    [tags] Find .h files with cscope too

--- home/.local/bin/tags-update.sh
+++ home/.local/bin/tags-update.sh
@@ -3,16 +3,18 @@
 DIR='.tags'
 mkdir -p $DIR
 
-# Only c/cpp/cc is important here. The YouCompleteMe will take care of the rest =)
+# Only c/cpp/cc is important for ctags. The YouCompleteMe/CSCOPE will take care of the rest =)
 if [[ "$1" == "c" ]]; then
-    find `pwd` -iname '*.c' | grep -v build\/ > $DIR/tags.files
+    find `pwd` -iname '*.c' | grep -v build\/ > $DIR/ctags.files
+    find `pwd` -iname '*.c' -o -iname '*.h' | grep -v build\/ > $DIR/cscope.files
     CTAGS_EXTRA_ARGS=
 else
-    find `pwd` -iname '*.cpp' -o -iname '*.cc' | grep -v build\/ > $DIR/tags.files
+    find `pwd` -iname '*.cpp' -o -iname '*.cc' | grep -v build\/ > $DIR/ctags.files
+    find `pwd` -iname '*.cpp' -o -iname '*.cc' -o -iname '*.h' | grep -v build\/ > $DIR/cscope.files
     CTAGS_EXTRA_ARGS="--language-force=C++"
 fi
 
-ctags --c++-kinds=+p --fields=+iaS --extra=+q $CTAGS_EXTRA_ARGS -L $DIR/tags.files -o $DIR/ctags
-cscope -b -q -i $DIR/tags.files
+ctags --c++-kinds=+p --fields=+iaS --extra=+q $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
+cscope -b -q -i $DIR/cscope.files
 mv cscope.* $DIR
 
