576e0b6e64a041494115d7bc1f5528a66f61c88f
Maxiwell Garcia <magarcia@padtec.com.br>
2017-05-18 11:02:58 -0300

    [vim] Fix vim-marks to avoid false-positive results

--- home/.local/bin/vim-marks.sh
+++ home/.local/bin/vim-marks.sh
@@ -2,6 +2,6 @@
 
 cat ~/.viminfo \
     | awk -v RS="History of marks" '{ print $0 NR }' \
-    | grep -e '^>.*$' -e '^[[:space:]][a-Z][[:space:]].*$' \
+    | grep -e '^>.*$' -e '^[[:space:]][a-Z][[:space:]][0-9].*$' \
     | grep -Pzo '(>.*\n[[:space:]].*\n)|([[:space:]][a-zA-Z][[:space:]].*\n)' 
 
