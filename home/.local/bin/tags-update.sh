30824e6cd1fb18d87d122a27aa85d4bccab6ab1b
Maxiwell Garcia <maxiwell@gmail.com>
2017-06-20 20:12:12 -0300

    [tags] Collecting files from many directories

--- home/.local/bin/tags-update.sh
+++ home/.local/bin/tags-update.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-DIR='.tags'
+DIR=`pwd`'/.tags'
 mkdir -p $DIR
 
-# Only c/cpp/cc is important for ctags. The YouCompleteMe/CSCOPE will take care of the rest =)
-if [[ "$1" == "c" ]]; then
-    find `pwd` -iname '*.c' | grep -v build\/ > $DIR/ctags.files
-    find `pwd` -iname '*.c' -o -iname '*.h' | grep -v build\/ > $DIR/cscope.files
-    CTAGS_EXTRA_ARGS=
-else
-    find `pwd` -iname '*.cpp' -o -iname '*.cc' | grep -v build\/ > $DIR/ctags.files
-    find `pwd` -iname '*.cpp' -o -iname '*.cc' -o -iname '*.h' | grep -v build\/ > $DIR/cscope.files
-    CTAGS_EXTRA_ARGS="--language-force=C++"
-fi
 
-ctags --c++-kinds=+p --fields=+iaS --extra=+q $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
-cscope -b -q -i $DIR/cscope.files
-mv cscope.* $DIR
+collecting_ctags() {
+    cd $1
+
+    # Only c/cpp/cc is important for ctags. The YouCompleteMe/CSCOPE will take care of the rest =)
+    find `pwd` -iname '*.c' | grep -v build\/ > $DIR/ctags.files
+    CTAGS_EXTRA_ARGS="--fields=+iaS --extra=+q"
+    ctags $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
+    
+    find `pwd` -iname '*.cpp' -o -iname '*.cc' | grep -v build\/ > $DIR/ctags.files
+    CTAGS_EXTRA_ARGS="--c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++"
+    ctags --append=yes $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
+
+    cd - &> /dev/null
+}
+
+
+collecting_cscope() {
+    cd $1
+
+    find `pwd` -iname '*.c' -o -iname '*.h' | grep -v build\/ > $DIR/cscope.files
+    find `pwd` -iname '*.cpp' -o -iname '*.cc' -o -iname '*.h' | grep -v build\/ >> $DIR/cscope.files
+    cscope -b -q -i $DIR/cscope.files
+    mv cscope.* $DIR
+
+    cd - &> /dev/null
+}
+
+
+for i in `pwd` $@; do
+    echo "Collecting from $i..."
+    collecting_ctags $i
+    collecting_cscope $i
+done
+
 
