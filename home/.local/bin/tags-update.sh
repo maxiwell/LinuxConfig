e073fcf5da3718614fe3009ba1a335136e4db99c
Maxiwell Garcia <magarcia@padtec.com.br>
2017-06-22 18:38:12 -0300

    [tags] Improve tag update script
    Change-Id: Ie5e85bc67c4a42c78788ae704ecf08ebe1fc9653

--- home/.local/bin/tags-update.sh
+++ home/.local/bin/tags-update.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 DIR=`pwd`'/.tags'
+rm -rf  ${DIR}_bkp
+mv $DIR ${DIR}_bkp
 mkdir -p $DIR
-
 
 collecting_ctags() {
     cd $1
 
-    # Only c/cpp/cc is important for ctags. The YouCompleteMe/CSCOPE will take care of the rest =)
-    find `pwd` -iname '*.c' | grep -v build\/ > $DIR/ctags.files
-    CTAGS_EXTRA_ARGS="--fields=+iaS --extra=+q"
-    ctags $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
-    
-    find `pwd` -iname '*.cpp' -o -iname '*.cc' | grep -v build\/ > $DIR/ctags.files
-    CTAGS_EXTRA_ARGS="--c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++"
-    ctags --append=yes $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
+    if [[ "$2" != "c++" ]]; then
+        # Only c/cpp/cc is important for ctags. The YouCompleteMe/CSCOPE will take care of the rest =)
+        find `pwd` -iname '*.c' | grep -v build\/ > $DIR/ctags.files
+        CTAGS_EXTRA_ARGS="--fields=+iaS --extra=+q"
+        ctags $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
+    fi
+
+    if [[ "$2" != "c" ]]; then
+        find `pwd` -iname '*.cpp' -o -iname '*.cc' | grep -v build\/ > $DIR/ctags.files
+        CTAGS_EXTRA_ARGS="--c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++"
+        ctags --append=yes $CTAGS_EXTRA_ARGS -L $DIR/ctags.files -o $DIR/ctags
+    fi
 
     cd - &> /dev/null
 }
@@ -23,8 +28,12 @@ collecting_ctags() {
 collecting_cscope() {
     cd $1
 
-    find `pwd` -iname '*.c' -o -iname '*.h' | grep -v build\/ > $DIR/cscope.files
-    find `pwd` -iname '*.cpp' -o -iname '*.cc' -o -iname '*.h' | grep -v build\/ >> $DIR/cscope.files
+    if [[ "$2" != "c++" ]]; then
+        find `pwd` -iname '*.c' -o -iname '*.h' | grep -v build\/ > $DIR/cscope.files
+    fi
+    if [[ "$2" != "c" ]]; then
+        find `pwd` -iname '*.cpp' -o -iname '*.cc' -o -iname '*.h' | grep -v build\/ >> $DIR/cscope.files
+    fi
     cscope -b -q -i $DIR/cscope.files
     mv cscope.* $DIR
 
@@ -32,10 +41,50 @@ collecting_cscope() {
 }
 
 
-for i in `pwd` $@; do
-    echo "Collecting from $i..."
-    collecting_ctags $i
-    collecting_cscope $i
+usage() {
+    echo "$(basename $0) <option> ./path_1 ./path_N"
+    echo ""
+    echo "   options:"
+    echo "      --only-c   : Find only .c and .h files"
+    echo "      --only-c++ : Find only .cpp, .cc, and .h files"
+    echo "      --all      : Find c and c++ files (default)"
+    echo ""
+}
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+ARGS="all"
+for i in $@; do
+
+    case "$i" in
+        --only-c)
+            ARGS="c"
+            shift
+            ;;
+        --only-c++)
+            ARGS="c++"
+            shift
+            ;;
+        --all)
+            shift
+            ;;
+        --help|-h)
+            usage
+            exit 0
+            ;;
+        -*)
+            echo "ERROR: option $i not found"
+            usage
+            exit 2
+            ;;
+        *)
+            echo "Collecting $ARGS files from $i"
+            collecting_ctags $i $ARGS
+            collecting_cscope $i $ARGS
+    esac
 done
 
 
