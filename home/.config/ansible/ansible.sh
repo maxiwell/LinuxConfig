408c48d163fe8d1ce0213284f30844d716e34d27
Maxiwell Garcia <maxiwell@gmail.com>
2019-01-24 12:09:46 -0200

    [ansible] Execute playbook based on OS type (ubuntu,fedora)

--- home/.config/ansible/ansible.sh
+++ home/.config/ansible/ansible.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-command -v ansible &>/dev/null || { echo -e "ERROR: Install 'ansible' package and try again: 
-                                            \n\tsudo apt-get install ansible\n"; exit 1; }
+command -v ansible &>/dev/null || { echo -e "ERROR: Install 'ansible' package\n"; exit 1; }
 
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+else
+    echo -e "ERROR: It's not possible identify the linux distribution"
+    exit 2;
+fi
 
-for i in *.yml; do
+for i in *-$OS.yml; do
     # FIXME: I use 'sudo' because 'pip' module fails  with 'become' privilege escalation
-    sudo ansible-playbook -i "localhost," -c local  $i --extra-vars "local_user=$USER local_home=$HOME" $1
+    sudo ansible-playbook -i "localhost," -c local  $i --extra-vars "local_user=$USER local_home=$HOME"
 done
 
