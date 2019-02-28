a36a0d41373513184ebd626d395db3f1c690753d
Maxiwell S. Garcia <maxiwell@linux.ibm.com>
2019-02-28 15:45:03 -0300

    [bin] Add offlineimap crontab note

--- home/.local/bin/offlineimap.sh
+++ home/.local/bin/offlineimap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# =================================
+# Add in the crontab -e
+
+# minutes hour dom month dow program
+# */10 * * * * $HOME/.local/bin/offlineimap.sh &>> /tmp/offlineimap.log
+# =================================
+
 if [ -f /tmp/offlineimap.lock ]; then
     echo "offlineimap.sh already in execution"
     exit 1;
