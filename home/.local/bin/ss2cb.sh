6fcf955c7672c46f476ac5c5627d38190383d782
Maxiwell S. Garcia <maxiwell@gmail.com>
2025-04-28 11:08:05 -0300

    replace scrot with flameshot

--- home/.local/bin/ss2cb.sh
+++ home/.local/bin/ss2cb.sh
@@ -4,6 +4,8 @@
 
 DIR=$HOME/screenshot
 
-mkdir -p $DIR
-scrot -s $DIR/foo.png
-xclip -t image/png -selection c -i $DIR/`ls -t $DIR | head -n1`
+#mkdir -p $DIR
+#scrot -s $DIR/foo.png
+#xclip -t image/png -selection c -i $DIR/`ls -t $DIR | head -n1`
+
+flameshot gui --path $DIR -c
